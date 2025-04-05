ac1fbc206196595301e439b8b1348ae7b6383873
zander.newcombe <zander.newcombe@gmail.com>
2025-04-05 02:53:22 +0000

    added page deploy & updated urls to be based of /blog-tutorial as home

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://zmnewk.github.io',
+    base: '/blog-tutorial'
+
+});
